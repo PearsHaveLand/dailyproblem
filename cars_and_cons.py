cdf05e3c0cadcb397b5bfcb36710cdaf5faad166
Pearce Haviland <pearce.haviland@protonmail.com>
2021-03-05 17:22:08 -0500

    Solved cons, car, and cdr problem

---
+++ cars_and_cons.py
@@ -0,0 +1,29 @@
+# cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first
+# and last element of that pair. For example, car(cons(3, 4)) returns 3, and
+# cdr(cons(3, 4)) returns 4.
+
+# Given this implementation of cons:
+
+# def cons(a, b):
+#     def pair(f):
+#         return f(a, b)
+#     return pair
+# Implement car and cdr.
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair):
+    def first_element(a, b):
+        return a
+    return pair(first_element)
+
+def cdr(pair):
+    def second_element(a,b):
+        return b
+    return pair(second_element)
+
+print(car(cons(3,4)))
+print(cdr(cons(3,4)))
